ad40a6164de2b2208a8891d0135d572e47449632
Chia-Hao Chung <jack.ntpc48@gmail.com>
2018-09-21 14:30:18 +0800

    Initial Commit

---
+++ HelloAlamoFireMoya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloAlamoFireMoya
+//
+//  Created by 鍾家豪 on 2018/9/21.
+//  Copyright © 2018年 鍾家豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
